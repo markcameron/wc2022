1e867ed04f171f0fc050f5a2195afb754b3702ca
Mark Cameron <mark.cameron@devfactory.ch>
2022-10-22 22:18:10 +0200

    Add Football API service config

--- config/services.php
+++ config/services.php
@@ -31,4 +31,10 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'football-api' => [
+        'key' => env('FOOTBALL_API_KEY'),
+        'url' => env('FOOTBALL_API_URL', 'https://v3.football.api-sports.io'),
+        'host' => env('FOOTBALL_API_HOST', 'v3.football.api-sports.io'),
+    ],
+
 ];
