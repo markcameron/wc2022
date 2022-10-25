942f7d4deaa4fa37b7c11a209b5e21a8df83af2c
Mark Cameron <mark.cameron@devfactory.ch>
2022-10-25 21:55:43 +0200

    Create predictions database migration

---
+++ database/migrations/2022_10_25_195245_create_predictions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('predictions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->integer('fixture_id')->unsigned()->index();
+            $table->foreign('fixture_id')->references('id')->on('fixtures')->onDelete('cascade');
+
+            $table->tinyInteger('score_home');
+            $table->tinyInteger('score_away');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('predictions');
+    }
+};
