03d6d10861c455e160e1d776bdf9d7fb70aa4999
Mark Cameron <mark.cameron@devfactory.ch>
2022-11-15 23:47:28 +0100

    Fix dark mode backgrounds for fixtures and predictions

--- resources/js/Pages/Predictions.vue
+++ resources/js/Pages/Predictions.vue
@@ -20,7 +20,7 @@ defineProps({
             <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
                     
                 <div v-for="fixture in fixtures"
-                    class="mb-2 py-2 px-4 rounded-lg bg-white border border-white dark:bg-gray-900 dark:border-gray-800 font-bold"
+                    class="mb-2 py-2 px-4 rounded-lg bg-white border border-white font-bold"
                     :class="{ 'bg-white': fixture.can_predict, 'bg-rose-50 border-l-8 border-orange-300': !fixture.can_predict }"
                 >
                     <Link :href="fixture.url_prediction">
