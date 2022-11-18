ff7778da5398030b70895203126b63fc235266cf
Mark Cameron <mark.cameron@devfactory.ch>
2022-11-18 21:27:21 +0100

    Attempt disabling cache for service worker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -71,14 +71,14 @@ self.addEventListener('activate', event => {
 });
 
 // Serve from Cache
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-            .catch((e) => {
-                return caches.match('offline');
-            })
-    )
-});
+// self.addEventListener("fetch", event => {
+//     event.respondWith(
+//         caches.match(event.request)
+//             .then(response => {
+//                 return response || fetch(event.request);
+//             })
+//             .catch((e) => {
+//                 return caches.match('offline');
+//             })
+//     )
+// });
