3bbec49e216fc49bb0ff2eda3880ef05e4cd70de
Mark Cameron <mark.cameron@devfactory.ch>
2022-11-20 16:21:21 +0100

    Optimizue Leaderboard

--- app/Http/Controllers/LeaderboardController.php
+++ app/Http/Controllers/LeaderboardController.php
@@ -10,7 +10,7 @@ class LeaderboardController extends Controller
 {
     public function index(Request $request)
     {
-        $users = User::get();
+        $users = User::with(['predictions.fixture.events'])->get();
 
         $users->transform(fn($user) => $user->append('score'));
 
